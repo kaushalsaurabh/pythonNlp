65e0f35277bd36d8f4c3fe42e1402129c3ced65f
Saurabh Kaushal <kaushalsaurabh@Saurabhs-MacBook-Pro-2.local>
2018-12-25 12:29:35 -0800

    Added program to read text from pdf

---
+++ nlp_with_python_cookbook/practice/user_defined_corpus/read_pdf.py
@@ -0,0 +1,26 @@
+'''This program reads pdf file'''
+from PyPDF2 import PdfFileReader
+import sys
+
+def get_text_from_pdf (pdf_file_name , password = ''):
+
+    # Initialize pdf file reader
+    pdf_file = open (pdf_file_name, 'rb')
+    read_pdf = PdfFileReader(pdf_file)
+
+    # Decrypt file with password
+    if password != '':
+        read_pdf.decrypt()
+
+    # Read the text from pdf pagewise and append
+    text = []
+    for i in range(0, read_pdf.getNumPages()-1):
+        text.append(read_pdf.getPage(i).extractText())
+
+    return '\n'.join(text)
+
+# Call the function with appropriate parameters
+print(get_text_from_pdf("Test.pdf"))
+
+
+
