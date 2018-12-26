adcadc820967af784fba5c8fb104a241bbff1e09
Saurabh Kaushal <kaushalsaurabh@Saurabhs-MacBook-Pro-2.local>
2018-12-25 16:56:56 -0800

    Added runs

--- nlp_with_python_cookbook/practice/user_defined_corpus/read_word.py
+++ nlp_with_python_cookbook/practice/user_defined_corpus/read_word.py
@@ -16,11 +16,21 @@ def get_text_from_word (filename):
 
     return '\n'.join(text)
 
+# Function to get multiple attributes in word file
 def get_attributes(filename):
+    # Create doc object
     doc = docx.Document(filename)
+
+    # Paragraphs
     print('Number of paragraphs', len(doc.paragraphs))
     print('Second Paragraph', doc.paragraphs[1].text)
     print('Second Paragraph style', doc.paragraphs[1].style)
+
+    # Runs. A run is continous text with same styling
+    print('Paragraph 1:', doc.paragraphs[0].text)
+    print('Number of runs in paragraph 1:', len(doc.paragraphs[0].runs))
+    for idx, run in enumerate(doc.paragraphs[0].runs):
+        print('Run %s : %s' % (idx, run.text))
 
 
 print(get_attributes('sample_docx_1page.docx'))
