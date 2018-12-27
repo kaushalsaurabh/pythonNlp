e60137f95143a320186a275a8d57a1a6f11a8443
Saurabh Kaushal <kaushalsaurabh@Saurabhs-MacBook-Pro-2.local>
2018-12-27 13:32:17 -0800

    Program to add custom stemmer

---
+++ nlp_with_python_cookbook/practice/regular_expression/custom_stemmer.py
@@ -0,0 +1,14 @@
+''' Program to create a custom stemmer'''
+import re
+
+def stem(word):
+    splits = re.findall(r'^(.*?)(ing|ly|ed|ious|ies|ive|es|s|ment)?$', word)
+    stem = splits[0][0]
+    return stem
+
+raw = "Keep your friends close, but your enemies closer."
+tokens = re.findall(r'\w+|\S\w*', raw)
+print(tokens)
+
+for t in tokens:
+  print(stem(t), end = ' ')
